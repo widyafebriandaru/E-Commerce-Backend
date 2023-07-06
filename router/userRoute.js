02efa372e65e4770b5562d760f8c6dccf50d3897
widyafebriandaru <76604668+widyafebriandaru@users.noreply.github.com>
2023-07-06 12:02:09 +0700

    fix register restriction

--- router/userRoute.js
+++ router/userRoute.js
@@ -11,7 +11,7 @@ const { verifyUser, adminOnly } = require("../middleware/authUser");
 
 router.get("/users", verifyUser, adminOnly, getUsers);
 router.get("/users/:id", verifyUser, adminOnly, getUserById);
-router.post("/users", verifyUser, createUser);
+router.post("/users", createUser);
 router.patch("/users/:id", verifyUser, adminOnly, updateUser);
 router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
 
